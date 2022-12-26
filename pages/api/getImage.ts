b361feb8419b87af5f0076195b0673572f9b2b24
jado66 <77120464+jado66@users.noreply.github.com>
2022-12-26 12:20:49 -0700

    Added image fetching from Dall E 2

---
+++ pages/api/getImage.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    const response = await openai.createImage({
+        prompt: req.body.prompt,
+        n: 1,
+        size: "256x256",
+  });
+
+  // console.log(JSON.stringify(completion,null,2))
+  res.status(200).json({ result: response.data });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,18 @@ export default function Home() {
     })
   }
 
-  const [story, setStory] = useState<string[]|null>(null)
+  // const [story, setStory] = useState<string[]|null>([null])
+  const [story, setStory] = useState<string[]|null>(
+  
+  [
+"    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec est sed nunc molestie venenatis vel id ligula. Donec eu lorem felis. Pellentesque eget faucibus risus. Integer at leo vel ex ullamcorper sagittis. Sed pulvinar lacus diam, id faucibus nibh malesuada id. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse lacinia ligula ipsum, ac luctus nisi placerat a. Ut venenatis lobortis tellus a luctus. Nulla sit amet leo faucibus nisi commodo consectetur ut in purus. Phasellus cursus egestas leo nec pretium. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+"Ut vitae ex mattis, mattis nulla eget, efficitur libero. Quisque facilisis, leo in aliquet volutpat, nisl nisl luctus mi, id aliquam felis turpis sed nunc. Vestibulum libero libero, hendrerit in lorem condimentum, blandit maximus tortor. Fusce metus risus, semper sit amet pulvinar eget, vulputate a neque. Nam eget fermentum tellus, in pretium est. Curabitur in nulla non mi laoreet cursus. Etiam fringilla mauris non vehicula porttitor. Nunc mattis lorem augue, sit amet imperdiet arcu pretium eu. Aliquam gravida ligula at elementum viverra. Phasellus eu nisl vel odio lobortis congue sed ut quam. Etiam dapibus in ipsum ut egestas.",
+"Fusce id vulputate lacus. Integer odio eros, vehicula at ornare vel, tempus vitae nisl. Maecenas id massa euismod, iaculis metus a, ornare odio. Duis enim dui, suscipit a nulla vitae, pellentesque suscipit libero. Integer viverra dapibus aliquet. Aenean condimentum lorem sed mi faucibus molestie. Sed sed quam pharetra, cursus nulla non, sagittis erat. Duis a lorem ac nunc gravida dictum id quis tellus.",
+"Aliquam erat volutpat. Vivamus congue fringilla pretium. Donec ipsum turpis, ultricies eu lorem ut, viverra laoreet velit. Etiam scelerisque lacus ut dui dapibus, a ornare ante convallis. Vivamus elementum tellus in velit semper, et aliquam mauris posuere. Etiam eget tincidunt elit, vel facilisis augue. Suspendisse eu dui et sem sollicitudin suscipit. Praesent nec lacinia eros. Nullam lacus odio, finibus ac tempus non, porttitor interdum massa. Sed a velit sagittis, venenatis turpis scelerisque, feugiat justo. Integer commodo metus quis turpis vehicula sollicitudin. Phasellus posuere fringilla leo, eu varius enim porta id. Nullam quis elementum turpis, vel condimentum enim. Donec vestibulum elit vel mauris ultrices, nec dictum eros hendrerit.",
+"Fusce placerat dolor at sapien dignissim rhoncus. Fusce faucibus sodales urna vitae ullamcorper. Nullam maximus orci id quam porta, ac pharetra ex dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam sapien felis, consectetur at finibus sed, vehicula vel orci. Donec eget quam eget justo feugiat laoreet ac in diam. Vivamus vehicula tempus nisl, id congue enim lacinia posuere. Integer rutrum eros magna, nec molestie nibh dictum ac. Aenean ornare fermentum augue ut dignissim. Aliquam accumsan interdum elit, sed malesuada augue consequat a. Nunc dui leo, tincidunt sit amet magna vel, volutpat tempor nibh."  
+])
+
+  const [storyImage, setStoryImage] = useState(null)
 
   const goBack = () => {
     setPrompt({
@@ -36,7 +47,15 @@ export default function Home() {
     setStory(null)
   }
 
-  const generatePrompt = () =>{
+  const generateImagePrompt = () =>{
+    if (!prompt.name || !prompt.subject || !prompt.message ){
+      return null
+    }
+    return `cute storybook illustration of a ${prompt.subject}`
+
+  }
+
+  const generateStoryPrompt = () =>{
     
     if (!prompt.name || !prompt.subject || !prompt.message ){
       return null
@@ -45,12 +64,42 @@ export default function Home() {
     return `Can create me a ${prompt.length} paragraph children's story about a ${prompt.subject.toLowerCase()} named ${prompt.name} with a hidden message about ${prompt.message}`
   }
 
-  const generateStory = useCallback(
+  const generateImage = useCallback(
     async () => {
       
-      const prompt = generatePrompt()
+      const prompt = generateImagePrompt()
 
       console.log("prompt: " +prompt)
+
+      if (!prompt){
+        return;
+      }
+      // setCompletion('Loading...');
+      const response = await fetch('/api/getImage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: prompt }),
+      });
+      const data = await response.json();
+
+      console.log(JSON.stringify(data ))
+      setStoryImage(data.result.data[0].url);
+      }
+  , [prompt]); 
+
+  const generateStory = () =>{
+    generateStoryText()
+    generateImage()
+  }
+
+  const generateStoryText = useCallback(
+    async () => {
+      
+      const prompt = generateStoryPrompt()
+
+      // console.log("prompt: " +prompt)
 
       if (!prompt){
         return;
@@ -65,7 +114,7 @@ export default function Home() {
       });
       const data = await response.json();
 
-      console.log(JSON.stringify(data ))
+      // console.log(JSON.stringify(data ))
       setStory(data.result.choices[0].text.split("\n\n"));
       }
   , [prompt]);
@@ -133,14 +182,29 @@ export default function Home() {
             <h1 className="text-center">{prompt.name} the {prompt.subject}</h1>
             <hr className="mt-4"/>  
 
+            {
+              storyImage ?
+              <div className="w-100 d-flex my-4 justify-content-center" style={{minHeight:200}}>
+                <img className = "bg-info border border-dark rounded-3" src={storyImage}/>
+              </div>
+              
+              :
+              <div className="w-100 bg-info border border-dark rounded-3 my-4" style={{minHeight:200}}></div>
+
+            }
+
+
             <div className="double-space fs-4">
               {
                 story.map((ele, index)=>{
                   return (
-                    <p key = {`p-${index}`}>
-                      {ele}
-                      
-                    </p>
+                    // <StoryBlock 
+                    //   index = {index}
+                    //   key = {`p-${index}`}
+                    //   text = {ele}  
+                    // />   
+                          <p key={'p-'+index}>{ele}</p>
+                 
                   )
                 })
               }
@@ -161,3 +225,19 @@ export default function Home() {
     </div>
   )
 }
+
+
+// const StoryBlock = (props:any) =>{
+//   return(
+//     <div>
+      
+//       {
+//         props.index === 0 && props.text.split(" ").length >50 &&
+//         <div className="w-100 bg-info border border-dark rounded-3 my-4" style={{minHeight:200}}></div>
+//       }
+      
+//       <p className="">{props.text}</p>
+//       {/* <p>-{props.text.split(" ").length} words</p> */}
+//     </div>
+//   )
+// }
