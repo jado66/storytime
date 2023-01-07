f04745d481c41abd7f8210391e3ecf01b3860ad7
jado66 <77120464+jado66@users.noreply.github.com>
2023-01-07 00:03:18 -0700

    Commented out broken code

--- components/book/Book.tsx
+++ components/book/Book.tsx
@@ -1,163 +1,163 @@
-import { useEffect, useRef, useState, useCallback } from "react";
-import Page from "./Page";
-import PageCover from "./PageCover";
-import HTMLFlipBook from "react-pageflip";
+// import { useEffect, useRef, useState, useCallback } from "react";
+// import Page from "./Page";
+// import PageCover from "./PageCover";
+// import HTMLFlipBook from "react-pageflip";
 
-export default function StoryBook(){
+// export default function StoryBook(){
 
-    const [pages, setPages] = useState<any>(null)
+//     const [pages, setPages] = useState<any>(null)
 
-    useEffect(()=>{
+//     useEffect(()=>{
 
   
   
-      const newPages = [
-        <PageCover key={0} pos="top">
+//       const newPages = [
+//         <PageCover key={0} pos="top">
           
-        </PageCover>
-      ];
+//         </PageCover>
+//       ];
   
-      let pageNum = 0;
-      for (let i = 0; i < 2; i++) {
-        pageNum++;
-        if (pageNum > 8) pageNum = 1;
-        newPages.push(
-          <Page key={i + 1} image={pageNum + ".jpg"} number={i + 1}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus
-            mollis nibh, non convallis ex convallis eu. Suspendisse potenti.
-            Aenean vitae pellentesque erat. Integer non tristique quam.
-            Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra
-            metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus,
-            accumsan eros sed, viverra enim. Pellentesque non justo vel nibh
-            sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. In cursus mollis nibh, non convallis ex
-            convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat.
-            Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin
-            mollis, eros velit viverra metus, a venenatis tellus tellus id magna.
-          </Page>
-        );
-      }
+//       let pageNum = 0;
+//       for (let i = 0; i < 2; i++) {
+//         pageNum++;
+//         if (pageNum > 8) pageNum = 1;
+//         newPages.push(
+//           <Page key={i + 1} image={pageNum + ".jpg"} number={i + 1}>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus
+//             mollis nibh, non convallis ex convallis eu. Suspendisse potenti.
+//             Aenean vitae pellentesque erat. Integer non tristique quam.
+//             Suspendisse rutrum, augue ac sollicitudin mollis, eros velit viverra
+//             metus, a venenatis tellus tellus id magna. Aliquam ac nulla rhoncus,
+//             accumsan eros sed, viverra enim. Pellentesque non justo vel nibh
+//             sollicitudin pharetra suscipit ut ipsum. Lorem ipsum dolor sit amet,
+//             consectetur adipiscing elit. In cursus mollis nibh, non convallis ex
+//             convallis eu. Suspendisse potenti. Aenean vitae pellentesque erat.
+//             Integer non tristique quam. Suspendisse rutrum, augue ac sollicitudin
+//             mollis, eros velit viverra metus, a venenatis tellus tellus id magna.
+//           </Page>
+//         );
+//       }
   
-      newPages.push(
-        <PageCover key={101} pos="bottom">
-          THE END
-        </PageCover>
-      );
+//       newPages.push(
+//         <PageCover key={101} pos="bottom">
+//           THE END
+//         </PageCover>
+//       );
 
-      setPages(newPages)
+//       setPages(newPages)
 
-    },[])
+//     },[])
 
-    const [page, setPage] = useState(0)
-    const [totalPage, setTotalPage] = useState(0)
-    const [orientation, setOrientation] = useState('landscape')
-    const [state, setState] = useState("read")
+//     const [page, setPage] = useState(0)
+//     const [totalPage, setTotalPage] = useState(0)
+//     const [orientation, setOrientation] = useState('landscape')
+//     const [state, setState] = useState("read")
     
-    const flipBook = useRef<any>(null)
+//     const flipBook = useRef<any>(null)
   
    
-    const nextButtonClick = useCallback(() => {
-        const pageFlipObj = flipBook.current.pageFlip();
+//     const nextButtonClick = useCallback(() => {
+//         const pageFlipObj = flipBook.current.pageFlip();
         
-        pageFlipObj.flipNext();
+//         pageFlipObj.flipNext();
         
         
         
-      }, [flipBook]);
+//       }, [flipBook]);
   
-    const prevButtonClick = () => {
-        const pageFlipObj = flipBook.current.pageFlip();
+//     const prevButtonClick = () => {
+//         const pageFlipObj = flipBook.current.pageFlip();
 
-        pageFlipObj.flipPrev();
-    };
+//         pageFlipObj.flipPrev();
+//     };
   
-    const onPage = (e:any) => {
-        setPage(e.data)
-    };
+//     const onPage = (e:any) => {
+//         setPage(e.data)
+//     };
   
-    const onChangeOrientation = (e:any) => {
-        setOrientation(e.data)
-    };
+//     const onChangeOrientation = (e:any) => {
+//         setOrientation(e.data)
+//     };
   
-    const onChangeState = (e:any) => {
-      setState(e.data)
-    };
+//     const onChangeState = (e:any) => {
+//       setState(e.data)
+//     };
   
-    useEffect(()=>{
+//     useEffect(()=>{
         
-        const pageFlipObj = flipBook.current.pageFlip();
-        if (pageFlipObj){
-            setTotalPage(pageFlipObj.getPageCount())
+//         const pageFlipObj = flipBook.current.pageFlip();
+//         if (pageFlipObj){
+//             setTotalPage(pageFlipObj.getPageCount())
 
-        }
-    }, [pages])
+//         }
+//     }, [pages])
     
    
   
-    return (
-        <>
-        <div className="h-100 d-flex" style={{ position: "relative" }}>
-            <HTMLFlipBook
-                width={550}
-                height={733}
-                size="stretch"
-                minWidth={115}
-                maxWidth={2000}
-                minHeight={100}
-                maxHeight={2533}
-                maxShadowOpacity={0.5}
-                showCover={true}
-                mobileScrollSupport={true}
-                onFlip={onPage}
-                onChangeOrientation={onChangeOrientation}
-                onChangeState={onChangeState}
-                className="flip-book html-book demo-book flex-grow-1"
-                // style={{ backgroundImage: "url(images/background.jpg)" }}
-                ref={flipBook} 
-                startPage={0} 
-                drawShadow={true} 
-                flippingTime={3000} 
-                usePortrait={false} 
-                startZIndex={0} 
-                autoSize={false} 
-                clickEventForward={false} 
-                useMouseEvents={false} 
-                swipeDistance={0} 
-                showPageCorners={true} 
-                disableFlipByClick={false}            >
-              {pages}
-            </HTMLFlipBook>
-          </div>
+//     return (
+//         <>
+//         <div className="h-100 d-flex" style={{ position: "relative" }}>
+//             <HTMLFlipBook
+//                 width={550}
+//                 height={733}
+//                 size="stretch"
+//                 minWidth={115}
+//                 maxWidth={2000}
+//                 minHeight={100}
+//                 maxHeight={2533}
+//                 maxShadowOpacity={0.5}
+//                 showCover={true}
+//                 mobileScrollSupport={true}
+//                 onFlip={onPage}
+//                 onChangeOrientation={onChangeOrientation}
+//                 onChangeState={onChangeState}
+//                 className="flip-book html-book demo-book flex-grow-1"
+//                 // style={{ backgroundImage: "url(images/background.jpg)" }}
+//                 ref={flipBook} 
+//                 startPage={0} 
+//                 drawShadow={true} 
+//                 flippingTime={3000} 
+//                 usePortrait={false} 
+//                 startZIndex={0} 
+//                 autoSize={false} 
+//                 clickEventForward={false} 
+//                 useMouseEvents={false} 
+//                 swipeDistance={0} 
+//                 showPageCorners={true} 
+//                 disableFlipByClick={false}            >
+//               {pages}
+//             </HTMLFlipBook>
+//           </div>
   
-          <div className="container mt-3">
-            <div className="row">
-              <div className="col-md-6">
-                <button
-                  type="button"
-                  className="btn btn-info btn-sm btn-prev"
-                  onClick={prevButtonClick}
-                >
-                  Previous page
-                </button>
-                [<span>{page + 1}</span> of{" "}
-                <span>{totalPage}</span>]
-                <button
-                  type="button"
-                  className="btn btn-info btn-sm btn-next"
-                  onClick={nextButtonClick}
-                >
-                  Next page
-                </button>
-              </div>
-              <div className="col-md-6">
-                State: <i>{state}</i>, orientation:{" "}
-                <i>{orientation}</i>
-              </div>
-            </div>
-        </div>
-        </>
+//           <div className="container mt-3">
+//             <div className="row">
+//               <div className="col-md-6">
+//                 <button
+//                   type="button"
+//                   className="btn btn-info btn-sm btn-prev"
+//                   onClick={prevButtonClick}
+//                 >
+//                   Previous page
+//                 </button>
+//                 [<span>{page + 1}</span> of{" "}
+//                 <span>{totalPage}</span>]
+//                 <button
+//                   type="button"
+//                   className="btn btn-info btn-sm btn-next"
+//                   onClick={nextButtonClick}
+//                 >
+//                   Next page
+//                 </button>
+//               </div>
+//               <div className="col-md-6">
+//                 State: <i>{state}</i>, orientation:{" "}
+//                 <i>{orientation}</i>
+//               </div>
+//             </div>
+//         </div>
+//         </>
           
-      );
+//       );
     
-  }
+//   }
   
